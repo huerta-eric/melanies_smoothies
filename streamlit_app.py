ac305e80bf454cb9821c79b0e23e355cd34747c3
Eric Huerta <ehuerta.g2a@gmail.com>
2024-03-21 11:39:24 -0600

    Update streamlit_app.py
    added dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,4 +41,4 @@ if ingredients_list:
         st.success(f"Thanks {name_on_order}! Your Smoothie is ordered.", icon="✅")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
