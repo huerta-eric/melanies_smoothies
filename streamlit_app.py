fc3ff1548cd3853ff707056dee40c1126b3084c8
Eric Huerta <ehuerta.g2a@gmail.com>
2024-03-21 11:37:02 -0600

    Update streamlit_app.py
    added requests library and requested the fruityvice api info

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title("My Parents New Healthy Diner")
@@ -38,3 +39,6 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success(f"Thanks {name_on_order}! Your Smoothie is ordered.", icon="✅")
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
